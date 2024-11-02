30c4960cfe6ba344719285cfbe573aa5a8d28a90
scake429 <scake429@gmail.com>
2024-11-03 01:10:07 +0400

    add corrections

--- test-mts/src/test/java/lesson_15/MtsTests.java
+++ test-mts/src/test/java/lesson_15/MtsTests.java
@@ -14,68 +14,83 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MtsTests {
     WebDriver driver;
+    WebDriverWait wait;
+
+    String tel = "297777777";
+    String sum = "100.00";
+    String email = "test@testmail.com";
+
+    WebElement sectPay;
 
     @BeforeAll
-    private static void setupClass() {
+    public static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
-    private void createDriver() {
+    public void createDriver() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get("https://www.mts.by/");
-    }
-
-    @AfterEach
-    private void teardown() {
-        driver.quit();
-    }
-
-    @Test
-    public void test() {
-        String tel;
-        String sum;
-        String email;
-        tel = "297777777";
-        sum = "100.00";
-        email = "test@testmail.com";
-
         driver.manage().deleteAllCookies();
 
         String title = driver.getTitle();
         assertEquals("МТС – мобильный оператор в Беларуси", title);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         try {
             WebElement butCookieCanc = driver.findElement(By.cssSelector(".cookie__wrapper .cookie__cancel"));
             wait.until(ExpectedConditions.elementToBeClickable(butCookieCanc));
             butCookieCanc.click();
-        } catch (TimeoutException e) {
+        } catch (Exception e) {
         }
 
-        WebElement sectPay = driver.findElement(By.cssSelector(".pay"));
+        sectPay = driver.findElement(By.cssSelector(".pay"));
+    }
 
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Checking the name of the replenishment block")
+    public void testCheckName() {
         WebElement heading = sectPay.findElement(By.tagName("h2"));
         assertEquals("Онлайн пополнение\nбез комиссии", heading.getText());
+    }
 
+    @Test
+    @DisplayName("Logo check")
+    public void testCheckLogo() {
         WebElement payPartnersList = sectPay.findElement(By.cssSelector(".pay__partners ul"));
-        assertEquals(payPartnersList.isDisplayed(), true);
+        assertTrue(payPartnersList.isDisplayed());
         assertEquals(payPartnersList.findElements(By.cssSelector("li>img")).size(), 5);
+    }
 
+    @Test
+    @DisplayName("Link check")
+    public void testCheckLink() {
         WebElement ancorDetailedServ = sectPay.findElement(By.cssSelector("a"));
-        assertEquals(ancorDetailedServ.isDisplayed(), true);
+        assertTrue(ancorDetailedServ.isDisplayed());
         assertEquals("Подробнее о сервисе", ancorDetailedServ.getText());
         assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/",
                 ancorDetailedServ.getAttribute("href"));
-
         ancorDetailedServ.click();
         assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/",
                 driver.getCurrentUrl());
+        String allText = driver.findElement(By.cssSelector("div.wrapper")).getText();
+        assertTrue(allText.contains("Оплата банковской картой"));
+        assertTrue(allText.contains("Информация о безопасности Интернет-платежей"));
+        assertTrue(allText.contains("Условия возврата денежных средств"));
         driver.navigate().back();
+    }
 
+    @Test
+    @DisplayName("Form check")
+    public void testCheckForm() {
         assertEquals("Услуги связи", driver.findElement(By.cssSelector("#pay-section .select__now")).getText());
         WebElement formPayments = driver.findElement(By.id("pay-connection"));
-        assertEquals(formPayments.isDisplayed(), true);
+        assertTrue(formPayments.isDisplayed());
         WebElement inputTel = formPayments.findElement(By.id("connection-phone"));
         inputTel.clear();
         inputTel.click();
@@ -95,13 +110,15 @@ public class MtsTests {
         assertEquals("Продолжить", butt.getText());
         butt.click();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        WebDriverWait waitForIframe = new WebDriverWait(driver, Duration.ofSeconds(10));
+        waitForIframe.until(ExpectedConditions.visibilityOfElementLocated(By.className("bepaid-iframe")));
 
         WebElement iframePayment = driver.findElement(By.className("bepaid-iframe"));
         driver.switchTo().frame(iframePayment);
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".app-wrapper__content")));
         WebElement wrapPayment = driver.findElement(By.cssSelector(".app-wrapper__content"));
+
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(), '" + sum + " BYN')]")));
         assertTrue(wrapPayment.findElement(By.xpath("//*[contains(text(), '" + sum + " BYN')]")).isDisplayed());
         assertTrue(wrapPayment
